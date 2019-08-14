ed5305c1c9c2fb36a3d3e9a17615142cabb36938
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-08-14 20:14:20 +0530

    Add files via upload

---
+++ CSE/Python3 Programs/swapThese.py
@@ -0,0 +1,15 @@
+
+# UPESx162
+# CSE / Python3 Programs
+# swapTheseNumbers
+# Recommended execution on https://repl.it/languages/python3
+
+if __name__=='__main__':
+	foo=int( input("Enter value for a: ") )
+	goo=int( input("Enter value for b: ") )
+	foo,goo=goo,foo
+	print("swapping values")
+	print("a is {0}, and b is {1}".format(foo,goo))
+
+# https://github.com/sarthakjain95
+# Sarthak Jain
---
+++ CSE/Python3 Programs/uultaNumber.py
@@ -0,0 +1,15 @@
+
+# UPESx162
+# CSE / Python3 Programs
+# reverseTheNumber
+# Recommended execution on https://repl.it/languages/python3
+
+if __name__=='__main__':
+	foo=input("Enter the number/string to be reversed: ")
+	if foo=='':
+		foo='  '
+	foo=foo[::-1]
+	print("Reversed Number/String is: \'"+foo+'\'')
+
+# https://github.com/sarthakjain95
+# Sarthak Jain
