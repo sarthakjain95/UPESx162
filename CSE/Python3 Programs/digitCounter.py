d1db637447539374562631d3ffe526f804fb63b1
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-08-14 23:01:40 +0530

    Add files via upload

---
+++ CSE/Python3 Programs/digitCounter.py
@@ -0,0 +1,12 @@
+
+# UPESx162
+# CSE / Python3 Programs
+# digitCounter
+# Recommended execution on https://repl.it/languages/python3
+
+if __name__=='__main__':
+	num=input("Enter a number/string: ")
+	print("{0} has {1} characters".format(num, len(num)))
+
+# https://github.com/sarthakjain95
+# Sarthak Jain
