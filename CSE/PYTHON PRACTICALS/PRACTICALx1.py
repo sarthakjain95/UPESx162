f13999687f5a709f0dfa7126ed1f6ddd0fcf634e
sarthakjain95 <saarthakjain95@gmail.com>
2019-09-01 12:15:08 +0530

    Added new practicals!

---
+++ CSE/PYTHON PRACTICALS/PRACTICALx1.py
@@ -0,0 +1,90 @@
+# CSE / PYTHON PRACTICALS
+# Practicals for 1st SEM CSE AI&ML [2019-2023] 
+
+# Q1) Say Hello to the user :
+#		Print Hello Everyone !!!
+print("Hello Everyone!")
+
+# Q2) Print Hello World in the following way :
+#		Hello
+#		World
+print("Hello\nWorld")
+
+
+# Q3) Print Hello World in the following way :
+#        	Hello
+#                	World
+print("Hello\n\tWorld")
+
+# Q4) say Hello to the user :
+#		Declare a string variable called x and assign it the value “Hello”.
+#		Print out the value of x
+x="Hello"
+print(x)
+
+# Q5) Print the following :
+#		‘ Brian’s mother : He’s not the messiah.
+#		He’s a very naughty boy ! ’
+print("Brian\'s mother: He\'s not the messiah.\nHe\'s a very naughty boy ! ")
+
+# Q6) Write a python programme to print the following string in a specific format (see the output)
+#	Output :
+#		Twinkle, Twinkle, little star,
+#			How I wonder What you are !
+#			Up above the world so high,
+#			Like a diamond in the sky.
+#		Twinkle, Twinkle, little star,
+#			How I wonder What you are.
+print("Twinkle, Twinkle, little star,")
+print("\tHow I wonder What you are !")
+print("\tUp above the world so high,")
+print("\tLike a diamond in the sky.")
+print("Twinkle, Twinkle, little star,")
+print("\tHow I wonder What you are.")
+
+#Q7) Take two variable a and b. Assign your first name and last name. Print your Name after adding your First name and Last name together.
+a="your_first_name"
+b="your_last_name"
+print( a + " " + b )
+
+#Q8) Give yourself a nickname :
+#	Declare three variables, consisting of your first name, your last name and Nickname. Write a  Program that prints out your first name, then your nickname in parenthesis and then your last name.  
+#	Example output : George ( woody ) Washington.
+a="your_first_name"
+b="your_last_name"
+c="Nickname"
+print("{0} ( {1} ) {2}.".format(a,c,b))
+
+#Q9) Print your name three times like
+#           GeorgeGeorgeGeorge
+a="your_name"
+print(a*3)
+
+#Q10) Print your name three times like 
+#            George       George        George
+a="your_name"
+print((a+'\t')*3)
+
+#Q11) Declare and assign values to the variables first name, last name, address, DOB, Programme, semester, SAP ID and print in the following way :
+#	NAME : NIKUNJ BANSAL
+# 	SAP ID : 500069944
+#	DATE OF BIRTH : 13 Oct 1999
+#	ADDRESS : UPES
+#		  Bidholi Campus
+#		  Pincode : 248007
+fname="your_first_name"
+lname="your_last_name"
+sem="1st"
+prog="B.TECH CSE AI&ML"
+sap="5x00012e2"
+dob="30 Feb 2001"
+address="UPES,\n\t Bidholi Campus\n\t Pincode:248007"
+print("NAME:",fname+" ",lname)
+print("SAD ID:",sap)
+print("DATE OF BIRTH:",dob)
+print("ADDRESS:",address)
+# print("PROGRAMME:",prog)
+# print("SEMESTER:",sem)
+
+# Sarthak Jain
+# https://github.com/sarthakjain95/UPESx162
