25608961514243d48c24307d82173616416a9f9f
sarthakjain95 <saarthakjain95@gmail.com>
2020-08-31 15:22:40 +0530

    Added files for Lab 3

---
+++ SEM III/Design and Analysis of Algorithms/Labs/03/README.md
@@ -0,0 +1,10 @@
+
+# Sorting Algorithms Comparison
+I've calculated and plotted the time differences for Bubble sort, Selection sort 
+and Merge sort in this program.
+
+### Note
+As you can see in the graph, the plot for Selection sort is always below that of 
+Bubble sort even though both of them have the same O(n^2) Complexity.  
+This is because Bubble sort swaps almost n\*(n-1) elements, while 
+Selection sort does only 'n' swaps for an array.
---
+++ SEM III/Design and Analysis of Algorithms/Labs/03/sorting_algorithms_comparison.c
@@ -0,0 +1,185 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define max_tests 32
+
+// Test results array
+int idx= 0;
+double bs_rec[max_tests];
+double ss_rec[max_tests];
+double ms_rec[max_tests];
+
+int * generate_random_array(int size){
+	//
+	return malloc( sizeof(int) * size );
+}
+
+void bubble_sort(int * arr, int size){
+
+	int temp;
+	//
+	for(int i=size; i>0; i--){
+		for(int j=0; j<i; j++){
+			if(arr[j] > arr[j+1]){
+				temp= arr[j];
+				arr[j]= arr[j+1];
+				arr[j+1]= temp;
+			}
+		}
+	}
+
+}
+
+void selection_sort(int * arr, int size){
+
+	int temp;
+	//
+	for(int i=0; i<size; i++){
+		int lowest_element_index= i;
+		for(int j=i; j<size; j++){
+			if( arr[j] < arr[lowest_element_index] ){
+				lowest_element_index= j;
+			}		
+		}
+		//
+		temp= arr[lowest_element_index];
+		arr[lowest_element_index]= arr[i];
+		arr[i]= temp;
+	}
+
+}
+
+void merge(int * arr, int start, int mid, int end){
+
+		int l_size= mid-start+1;
+		int r_size= end-mid;
+		int * l_arr= malloc( sizeof(int) * l_size );
+		int * r_arr= malloc( sizeof(int) * r_size );
+		//
+		for(int i=0; i<l_size; i++) l_arr[i]= arr[start+i];
+		for(int i=0; i<r_size; i++) r_arr[i]= arr[mid+i+1];
+		//
+		int i, j, k;
+		i= 0;
+		j= 0;
+		k= start;
+		//
+		while(i<l_size && j<r_size){
+			//
+			if(l_arr[i]<r_arr[j]){
+				arr[k]= l_arr[i];
+				i++;
+			}else{
+				arr[k]= r_arr[j];
+				j++;
+			}
+			//
+			k++;
+		}
+		//
+		while(i<l_size){
+			arr[k]= l_arr[i];
+			i++;
+			k++;
+		}
+		//
+		while(j<r_size){
+			arr[k]= r_arr[j];
+			j++;
+			k++;
+		}
+		
+}
+
+void merge_sort(int * arr, int start, int end){
+
+	if(start<end){
+		int mid= start + (end-start)/2;
+		//
+		// Sort sub arrays
+		merge_sort(arr, start, mid);
+		merge_sort(arr, mid+1, end);
+		//
+		// Combine/Merge the two sorted sub arrays
+		merge(arr, start, mid, end);
+	}
+
+}
+
+int sorting_tests(int size){
+
+	int * bst1= generate_random_array(size);
+	int * sst1= generate_random_array(size);
+	int * mst1= generate_random_array(size);
+	//
+	int n=0;
+	for(int i=0; i<size; i++){
+		n= rand() % size;
+		bst1[i]= n;
+		sst1[i]= n;
+		mst1[i]= n;
+	}
+	//
+	clock_t start, end;
+	double time_taken= 0;
+	//
+	printf("Tests for %d elements\n", size);
+	//
+	start= clock();
+	bubble_sort(bst1, size);	
+	end= clock();
+	time_taken= ( (end-start) * 10e3 ) / CLOCKS_PER_SEC;
+	printf("Bubble sort took %lf micro sec\n", time_taken);
+	bs_rec[idx]= time_taken; // saving results
+	//
+	start= clock();
+	selection_sort(sst1, size);	
+	end= clock();
+	time_taken= ( (end-start) * 10e3 ) / CLOCKS_PER_SEC;
+	printf("Selection sort took %lf micro sec\n", time_taken);
+	ss_rec[idx]= time_taken;
+	//
+	start= clock();
+	merge_sort(mst1, 0, size);	
+	end= clock();
+	time_taken= ( (end-start) * 10e3 ) / CLOCKS_PER_SEC;
+	printf("Merge sort took %lf micro sec\n", time_taken);
+	ms_rec[idx]= time_taken;
+	//
+	idx++; // incrementing results array index
+	printf("\n");
+}
+
+int main(){
+
+	// constant seed
+	srand(42);
+	//
+	sorting_tests(10000);
+	sorting_tests(20000);
+	sorting_tests(30000);
+	sorting_tests(40000);
+	sorting_tests(50000);
+	sorting_tests(60000);
+	sorting_tests(70000);
+	sorting_tests(80000);
+	sorting_tests(90000);
+
+	// Uncomment for getting plot arrays
+	//
+	// printf("Bubble sort: [");
+	// for(int i=0; i<idx; i++) printf("%.2lf, ", bs_rec[i]);
+	// printf("]\n");
+	//
+	// printf("Selection sort: [");
+	// for(int i=0; i<idx; i++) printf("%.2lf, ", ss_rec[i]);
+	// printf("]\n");
+	//
+	// printf("Merge sort: [");
+	// for(int i=0; i<idx; i++) printf("%.2lf, ", ms_rec[i]);
+	// printf("]\n");
+
+	return 0;
+}
